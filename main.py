8ce59d231bd711f7b0db87b04b9233fa927b9005
Huawei_station <smile56680@gmail.com>
2022-10-31 17:18:20 +0300

    second commit

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from flask import Flask
 from utils import load_candidates_from_json, get_candidate, get_candidates_by_name, get_candidates_by_skill
 
+app = Flask(__name__)
+
 PATH_FILE = 'candidates.json'
 candidates_data = load_candidates_from_json(PATH_FILE)
+
+
+@app.route('/')
+def candidates():
+    candidates_data_return = ['<h1>Все кандидаты</h1>']
+    for candidate in candidates_data:
+        candidate_data = f'<p><a href="/candidate/<x>">{candidate["picture"]}</a></p>'
+
